c9ad2bd3088e3e9cd80c1ea8496150edf26f2c7f
Giuseppe <giuseppe.martino26@outlook.it>
2022-03-18 18:40:15 +0100

    sistemazioni

--- Login.py
+++ Login.py
@@ -34,52 +34,52 @@ class Login:
         self.label_img = Label(self.root, image=self.img)
         self.label_img.grid(row=0, column=0, columnspan=3)
 
-        global frame
+        #global frame
 
-        frame = LabelFrame(self.root, pady=100, padx=100)
-        frame.grid(row=2, column=1)
+        self.frame = LabelFrame(self.root, pady=100, padx=100)
+        self.frame.grid(row=2, column=1)
 
         self.frame2 = LabelFrame(self.root, pady=100, padx=100)
 
         myFont = font.Font(family='Calibri', size=12)
-        user_label = Label(frame, text="Username: ",font=myFont)
-        pass_label = Label(frame, text="Password: ",font=myFont)
-        space_label1 = Label(frame, text=" ")
-        space_label2 = Label(frame, text=" ")
-        user_entry = Entry(frame, width=17,font=myFont)
-        pass_entry = Entry(frame,width=17,font=myFont, show='*')
+        user_label = Label(self.frame, text="Username: ",font=myFont)
+        pass_label = Label(self.frame, text="Password: ",font=myFont)
+        space_label1 = Label(self.frame, text=" ")
+        space_label2 = Label(self.frame, text=" ")
+        user_entry = Entry(self.frame, width=17,font=myFont)
+        pass_entry = Entry(self.frame,width=17,font=myFont, show='*')
         user_label.grid(row=2, column=1)
         user_entry.grid(row=2, column=2)
         space_label1.grid(row=3, column=1)
         pass_label.grid(row=4, column=1)
         pass_entry.grid(row=4, column=2)
-        login_button = Button(frame, text="Login", padx=20, pady=7, bg="#4eb6b0", font=myFont, command=lambda: self.funzione(str(user_entry.get())))
+        login_button = Button(self.frame, text="Login", padx=20, pady=7, bg="#4eb6b0", font=myFont, command=lambda: self.funzione(str(user_entry.get())))
 
         space_label2.grid(row=5, column=1)
         login_button.grid(row=6, column=2)
 
-        space_label5 = Label(frame, text="\n" + "\n")
+        space_label5 = Label(self.frame, text="\n" + "\n")
         space_label5.grid(row=7, column=1)
 
-        user_label2 = Label(frame, text="Username: ",font=myFont)
-        pass_label2 = Label(frame, text="Password: ", font=myFont)
-        space_label3 = Label(frame, text=" ")
-        space_label4 = Label(frame, text=" ")
-        user_entry2 = Entry(frame, width=17,font=myFont)
-        pass_entry2 = Entry(frame, width=17,font=myFont)
+        user_label2 = Label(self.frame, text="Username: ",font=myFont)
+        pass_label2 = Label(self.frame, text="Password: ", font=myFont)
+        space_label3 = Label(self.frame, text=" ")
+        space_label4 = Label(self.frame, text=" ")
+        user_entry2 = Entry(self.frame, width=17,font=myFont)
+        pass_entry2 = Entry(self.frame, width=17,font=myFont)
         user_label2.grid(row=8, column=1)
         user_entry2.grid(row=8, column=2)
         space_label3.grid(row=9, column=1)
         pass_label2.grid(row=10, column=1)
         pass_entry2.grid(row=10, column=2)
-        login_button2 = Button(frame, text="Register", padx=20, pady=7, bg="#1d434e", fg="white",font=myFont)
+        login_button2 = Button(self.frame, text="Register", padx=20, pady=7, bg="#1d434e", fg="white",font=myFont)
         space_label4.grid(row=11, column=1)
         login_button2.grid(row=12, column=2)
 
-        space_label6 = Label(frame, text="                                             ")
+        space_label6 = Label(self.frame, text="                                             ")
         # space_label6.grid(row=1,column=4)
 
-        space_label13 = Label(frame, text="\n" + "\n")
+        space_label13 = Label(self.frame, text="\n" + "\n")
 
 
 
--- Panel.py
+++ Panel.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import tkinter.font as font
 
 
 
@@ -8,10 +9,11 @@ class Panel:
         #self.root = root
         self.frame = frame
 
-
-        label_insert_p = Label(frame, text="Insert a new player to the team")
-        entry_insert_p = Entry(frame, width=25)
-        button_insert_p = Button(frame, text="Insert", padx=25, pady=5, bg="#3bab5a", fg="white")
+        myFont = font.Font(family='Calibri', size=14)
+        myFont2 = font.Font(family='Calibri', size=12)
+        label_insert_p = Label(frame, text="Insert a new player to the team",font=myFont)
+        entry_insert_p = Entry(frame, width=25, font=myFont2)
+        button_insert_p = Button(frame, text="Insert", padx=25, pady=5, bg="#3bab5a", fg="white",font=myFont2)
         space_label1 = Label(frame, text=" ")
         space_label2 = Label(frame, text=" ")
         space_label3 = Label(frame, text="\n"+"\n")
@@ -23,18 +25,18 @@ class Panel:
         button_insert_p.grid(row=4,column=2)
         space_label3.grid(row=7, column=1)
 
-        rating_button = Button(frame, text="Rate a player",padx=25, pady=10, bg="#1d434e", fg="white")
+        rating_button = Button(frame, text="Rate a player",padx=25, pady=10, bg="#1d434e", fg="white",font=myFont2)
         rating_button.grid(row=8, column=1)
         space_label4 = Label(frame, text="\n" + "\n")
         space_label4.grid(row=9,column=1)
 
-        lineup_button = Button(frame, text="View the starting lineup", padx=25, pady=10, bg="#1d434e", fg="white")
+        lineup_button = Button(frame, text="View the starting lineup", padx=25, pady=10, bg="#1d434e", fg="white",font=myFont2)
         lineup_button.grid(row=10, column=1)
         space_label5 = Label(frame, text="\n" + "\n")
         space_label5.grid(row=11, column=1)
 
 
-        trend_button = Button(frame, text="View a player's performance trend", padx=25, pady=10, bg="#1d434e", fg="white",command= self.indietro)
+        trend_button = Button(frame, text="View a player's performance trend", padx=25, pady=10, bg="#1d434e", fg="white",font=myFont2,command= self.indietro)
         trend_button.grid(row=12, column=1)
 
     def indietro(self):
