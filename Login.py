663380df999791d7583445093958fc08adb5912e
Giuseppe <giuseppe.martino26@outlook.it>
2022-03-18 14:47:09 +0100

    panel

--- Login.py
+++ Login.py
@@ -2,9 +2,28 @@ from tkinter import *
 from PIL import ImageTk,Image
 
 from Panel import Panel
+from tkinter import messagebox
+
+
 
 
 class Login:
+
+    def funzione(self,testo):
+        if testo == "ciao":
+            messagebox.showerror("Error", "Credentials not valid, please retry.")
+
+        if testo == "hola":
+            self.frame.grid_forget()
+            Panel(self.root, self.frame2)
+            self.frame2.grid(row=2, column=1)
+
+
+    def twotoone(self):
+        self.frame2.grid_forget()
+        self.frame.grid(row=2, column=1)
+
+
     def __init__(self):
 
         self.root = Tk()
@@ -20,9 +39,50 @@ class Login:
         self.frame = LabelFrame(self.root, pady=100, padx=100)
         self.frame.grid(row=2, column=1)
 
-        self.panel = Panel(self.root, self.frame)
+        self.frame2 = LabelFrame(self.root, pady=100, padx=100)
+
+        #self.panel = Panel(self.root, self.frame)
+        user_label = Label(self.frame, text="Username")
+        pass_label = Label(self.frame, text="Password")
+        space_label1 = Label(self.frame, text=" ")
+        space_label2 = Label(self.frame, text=" ")
+        user_entry = Entry(self.frame, width=25)
+        pass_entry = Entry(self.frame, width=25)
+        user_label.grid(row=2, column=1)
+        user_entry.grid(row=2, column=2)
+        space_label1.grid(row=3, column=1)
+        pass_label.grid(row=4, column=1)
+        pass_entry.grid(row=4, column=2)
+        login_button = Button(self.frame, text="Login", padx=25, pady=10, bg="#4eb6b0", command=lambda: self.funzione(str(user_entry.get())))
+        space_label2.grid(row=5, column=1)
+        login_button.grid(row=6, column=2)
+
+        space_label5 = Label(self.frame, text="\n" + "\n")
+        space_label5.grid(row=7, column=1)
+
+        user_label2 = Label(self.frame, text="Username")
+        pass_label2 = Label(self.frame, text="Password")
+        space_label3 = Label(self.frame, text=" ")
+        space_label4 = Label(self.frame, text=" ")
+        user_entry2 = Entry(self.frame, width=25)
+        pass_entry2 = Entry(self.frame, width=25)
+        user_label2.grid(row=8, column=1)
+        user_entry2.grid(row=8, column=2)
+        space_label3.grid(row=9, column=1)
+        pass_label2.grid(row=10, column=1)
+        pass_entry2.grid(row=10, column=2)
+        login_button2 = Button(self.frame, text="Register", padx=25, pady=10, bg="#1d434e", fg="white")
+        space_label4.grid(row=11, column=1)
+        login_button2.grid(row=12, column=2)
+
+        space_label6 = Label(self.frame, text="                                             ")
+        # space_label6.grid(row=1,column=4)
+
+        space_label13 = Label(self.frame, text="\n" + "\n")
 
 
 
     def start(self):
         self.root.mainloop()
+
+
--- Panel.py
+++ Panel.py
@@ -1,46 +1,51 @@
 from tkinter import *
 
+#import Login
+
+from Login import *
+#from Login import Login
+
+
 class Panel:
+
+
+
+
     def __init__(self, root, frame):
         self.root = root
         self.frame = frame
 
-        #Label(self.frame, text="ciao").pack()
 
-        user_label = Label(frame, text="Username")
-        pass_label = Label(frame, text="Password")
+
+        label_insert_p = Label(frame, text="Insert a new player to the team")
+        entry_insert_p = Entry(frame, width=25)
+        button_insert_p = Button(frame, text="Insert", padx=25, pady=5, bg="#3bab5a", fg="white")
         space_label1 = Label(frame, text=" ")
         space_label2 = Label(frame, text=" ")
-        user_entry = Entry(frame, width=25)
-        pass_entry = Entry(frame, width=25)
-        user_label.grid(row=2, column=1)
-        user_entry.grid(row=2, column=2)
-        space_label1.grid(row=3, column=1)
-        pass_label.grid(row=4, column=1)
-        pass_entry.grid(row=4, column=2)
-        login_button = Button(frame, text="Login", padx=25, pady=10, bg="#4eb6b0", command=lambda: funzione(str(user_entry.get())))
-        space_label2.grid(row=5, column=1)
-        login_button.grid(row=6, column=2)
+        space_label3 = Label(frame, text="\n"+"\n")
 
+        label_insert_p.grid(row=2, column=1,columnspan=2)
+        space_label1.grid(row=3,column=1)
+        entry_insert_p.grid(row=4,column=1)
+        space_label2.grid(row=5,column=1)
+        button_insert_p.grid(row=4,column=2)
+        space_label3.grid(row=7, column=1)
+
+        rating_button = Button(frame, text="Rate a player",padx=25, pady=10, bg="#1d434e", fg="white")
+        rating_button.grid(row=8, column=1)
+        space_label4 = Label(frame, text="\n" + "\n")
+        space_label4.grid(row=9,column=1)
+
+        lineup_button = Button(frame, text="View the starting lineup", padx=25, pady=10, bg="#1d434e", fg="white")
+        lineup_button.grid(row=10, column=1)
         space_label5 = Label(frame, text="\n" + "\n")
-        space_label5.grid(row=7, column=1)
+        space_label5.grid(row=11, column=1)
 
-        user_label2 = Label(frame, text="Username")
-        pass_label2 = Label(frame, text="Password")
-        space_label3 = Label(frame, text=" ")
-        space_label4 = Label(frame, text=" ")
-        user_entry2 = Entry(frame, width=25)
-        pass_entry2 = Entry(frame, width=25)
-        user_label2.grid(row=8, column=1)
-        user_entry2.grid(row=8, column=2)
-        space_label3.grid(row=9, column=1)
-        pass_label2.grid(row=10, column=1)
-        pass_entry2.grid(row=10, column=2)
-        login_button2 = Button(frame, text="Register", padx=25, pady=10, bg="#1d434e", fg="white")
-        space_label4.grid(row=11, column=1)
-        login_button2.grid(row=12, column=2)
 
-        space_label6 = Label(frame, text="                                             ")
-        # space_label6.grid(row=1,column=4)
+        trend_button = Button(frame, text="View a player's performance trend", padx=25, pady=10, bg="#1d434e", fg="white")
+        trend_button.grid(row=12, column=1)
 
-        space_label13 = Label(frame, text="\n" + "\n")
+
+
+
+
