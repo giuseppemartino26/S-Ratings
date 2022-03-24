e75a05868ecb85ff18fc4ebfaeeafea1f652db96
Giuseppe <giuseppe.martino26@outlook.it>
2022-03-24 20:32:04 +0100

    formazioni finite

--- Lineup.py
+++ Lineup.py
@@ -35,6 +35,7 @@ class Lineup:
 
         if choice == '4-4-2':
             self.frame_442.grid_forget()
+            self.frame_352.grid_forget()
             fw_list = self.compute_list(self.username,'Forewarder', 2)
             print(fw_list)
 
@@ -78,6 +79,7 @@ class Lineup:
 
         if choice == '4-3-3':
             self.frame_442.grid_forget()
+            self.frame_352.grid_forget()
             fw_list = self.compute_list(self.username,'Forewarder', 3)
             print(fw_list)
 
@@ -120,6 +122,51 @@ class Lineup:
             self.frame_433.tkraise()
 
 
+        if choice == '3-5-2':
+            self.frame_442.grid_forget()
+            self.frame_433.grid_forget()
+            fw_list = self.compute_list(self.username,'Forewarder', 2)
+
+
+            Label(self.frame_352, image=self.img_g).place(anchor='center', relx=0.5, rely=0.5)
+
+            Label(self.frame_352, text= fw_list[0][0] +"\n"+"("+ str(fw_list[0][1])+")",font=self.myFont2).grid(row=6, column=0, columnspan=2)
+            Label(self.frame_352, text=fw_list[1][0] + "\n" + "(" + str(fw_list[1][1]) + ")", font=self.myFont2).grid(row=6, column=4, columnspan=2)
+
+
+            Label(self.frame_352, text="\n"+"\n"+"\n"+"\n"+"\n",bg="#7ec576").grid(row=5, column=0)
+
+            mf_list = self.compute_list(self.username, 'Midfielder', 5)
+            Label(self.frame_352, text=mf_list[0][0]+"\n"+"("+ str(mf_list[0][1])+")", font=self.myFont2).grid(row=4, column=0)
+
+            Label(self.frame_352, text=mf_list[1][0]+"\n"+"("+ str(mf_list[1][1])+")", font=self.myFont2).grid(row=4, column=2)
+
+            Label(self.frame_352, text=mf_list[2][0]+"\n"+"("+ str(mf_list[2][1])+")", font=self.myFont2).grid(row=4, column=4)
+
+            Label(self.frame_352, text=mf_list[3][0] + "\n" + "(" + str(mf_list[3][1]) + ")", font=self.myFont2).grid(row=5, column=0, columnspan=2)
+            Label(self.frame_352, text=mf_list[4][0] + "\n" + "(" + str(mf_list[4][1]) + ")", font=self.myFont2).grid(row=5, column=4, columnspan=2)
+
+
+            Label(self.frame_352, text="\n" + "\n",bg="#7ec576").grid(row=3, column=0)
+
+            df_list = self.compute_list(self.username, 'Defensor',3)
+            Label(self.frame_352, text=df_list[0][0]+"\n"+"("+ str(df_list[0][1])+")", font=self.myFont2).grid(row=2, column=0)
+            Label(self.frame_442, text="  ", bg="#7ec576").grid(row=2, column=1)
+            Label(self.frame_352, text=df_list[1][0]+"\n"+"("+ str(df_list[1][1])+")", font=self.myFont2).grid(row=2, column=2)
+            Label(self.frame_442, text="  ", bg="#7ec576").grid(row=2, column=2)
+            Label(self.frame_352, text=df_list[2][0]+"\n"+"("+ str(df_list[2][1])+")", font=self.myFont2).grid(row=2, column=4)
+
+
+            Label(self.frame_352, text="\n" + "\n",bg="#7ec576").grid(row=1, column=0)
+
+            gk_list = self.compute_list(self.username, 'Goalkeeper', 1)
+            Label(self.frame_352, text=gk_list[0][0]+"\n"+"("+ str(gk_list[0][1])+")", font=self.myFont2).grid(row=0, column=2)
+
+
+            self.frame_352.grid(row=3, column=1)
+            self.frame_352.tkraise()
+
+
 
 
 
@@ -136,6 +183,7 @@ class Lineup:
 
         self.frame_442 = LabelFrame(self.frame, pady=90, padx=5)
         self.frame_433 = LabelFrame(self.frame, pady=90, padx=25)
+        self.frame_352 = LabelFrame(self.frame, pady=70, padx=100)
 
 
         image = Image.open("back2.png")
